bda5e047e0c6d2d69fc35623ef115361a78d5bb1
Jorge Luis Santiago Alvarez <jorgeluis@bunsan.io>
2018-05-06 22:10:53 -0500

    Add homework 1

---
+++ homework_1/jorge/lib/jorge.ex
@@ -0,0 +1,18 @@
+defmodule Jorge do
+  @moduledoc """
+  Documentation for Jorge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jorge.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
